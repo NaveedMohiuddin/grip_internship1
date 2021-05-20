f913a6fdc2c2e1fc2acafb08bcb1717f80517fe7
NaveedMohiuddin <naveed.mohiuddin.1234@gmail.com>
2021-05-20 17:21:20 +0530

    git commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../index.css';
+
+
+const Footer = () => {
+    return(
+        <>
+            <div className="footer">
+                <h3 className="footerContent">Developed by Naveed Mohiuddin | 2021 </h3>
+            </div>
+        </>
+    );
+}
+
+export default Footer;
---
+++ src/Components/Info.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Info = () => {
+    return (
+        <>
+            <div className="info">
+                <h2>Naveed Mohiuddin</h2><br></br>
+                <h3>Task : #1</h3>
+                <h3>Web Development</h3><br></br>
+                <h3>Graduate Rotational Internship Program</h3>
+            </div>
+        </>
+    );
+}
+
+export default Info;
